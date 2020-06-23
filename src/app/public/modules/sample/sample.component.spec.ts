42994929ef3912e9e0532dfbac0020e37acfb9bd
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2020-06-23 14:31:36 -0400

    Fix tests

--- src/app/public/modules/sample/sample.component.spec.ts
+++ src/app/public/modules/sample/sample.component.spec.ts
@@ -33,7 +33,7 @@ describe('LibrarySampleComponent', () => {
 
   it('should output the name from config', () => {
     fixture.detectChanges();
-    expect(component.appSettings.myLibrary.name).toBe('Invalid.');
+    expect(component.appSettings.myLibrary.name).toBe('My Library');
   });
 
   it('should pass accessibility', async(() => {
